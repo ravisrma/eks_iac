d08e468ae6c2d096dacf69acacef635d691e6a8a
Ravi Sharma <ravisharma0527@gmail.com>
2025-01-29 12:29:14 +0530

    update

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  backend "s3" {
+    bucket         = "eks-terraform-bucket0"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+  }
+}
+# Creating VPC
+module "vpc" {
+  source       = "./modules/vpc"
+  cluster_name = var.cluster_name
+}
+
+#Creating security group
+module "security_groups" {
+  source       = "./modules/security"
+  vpc_id       = module.vpc.vpc_id
+  cluster_name = var.cluster_name
+} 
+
+# }
+# Creating IAM resources
+module "iam" {
+  source = "./modules/iam"
+}
+
+
+
+# Creating EKS Cluster
+module "eks" {
+  source = "./modules/eks"
+  master_arn = module.iam.master_arn
+  worker_arn = module.iam.worker_arn
+  instance_size = var.instance_size
+  desired_size = var.desired_size
+  max_size = var.max_size
+  min_size = var.min_size
+  cluster_name = var.cluster_name
+  cluster_version = var.cluster_version
+  public_subnet_az1_id = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id = module.vpc.public_subnet_az2_id
+  eks_cluster_sg_id = module.security_groups.eks_cluster_sg_id
+  eks_nodes_sg_id = module.security_groups.eks_nodes_sg_id
+  vpc_id = module.vpc.vpc_id
+  key_name = var.key_name
+  amazon_ebs_csi_driver_version = var.amazon_ebs_csi_driver_version
+  vpc-cni-version = var.vpc-cni-version
+  kube-proxy-version = var.kube-proxy-version
+  coredns-version = var.coredns-version
+}
---
+++ terraform.tfvars
@@ -0,0 +1,12 @@
+cluster_name       = "eks-cluster"
+instance_size      = "t2.medium"
+desired_size       = 2
+max_size           = 5
+min_size           = 2
+region             = "ap-south-1"
+cluster_version    = "1.31"
+vpc-cni-version    = "v1.19.2-eksbuild.1"
+kube-proxy-version = "v1.31.3-eksbuild.2"
+coredns-version    = "v1.11.4-eksbuild.1"
+amazon_ebs_csi_driver_version = "v1.37.0-eksbuild.1"
+key_name           = "test"
